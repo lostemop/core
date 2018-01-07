6cf8de19d5a2b38c079967587e0a097de435f980
kevin <284299280@qq.com>
2018-01-07 15:31:49 +0800

    core

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevins
+ * Date: 2018/1/6
+ * Time: 20:56
+ */
+return  [
+    '@vendor' => dirname(__FILE__).'/vendor',
+    '@bower' => dirname(__FILE__).'/vendor/bower-asset',
+];
